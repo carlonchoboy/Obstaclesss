64afaf61aa23f7ed37ed633de28876abc70a50c1
Carlos Andrés Chiale <carloncho@thechfactory.com>
2020-12-07 00:55:23 -0300

    PlayerController

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //public float moveSpeed = 5f;
+
+    public float maxSpeed = 10f;
+
+    public float acceleration = 1f;
+
+    public float decelartion = 1f;
+
+    public float jumpForce = 10f;
+
+    public LayerMask Ground;
+
+    private float speed = 0f;
+
+    private Rigidbody rigidbody;
+
+    private Vector3 moveDirection;
+
+    private float distToGround;
+
+    private Collider collider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        collider = GetComponent<Collider>();
+        distToGround = collider.bounds.extents.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckJump();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.X))
+        {
+            speed = speed - acceleration * Time.deltaTime;
+            //Vector3 temp = new Vector3(1, 0, 0);
+            //temp = temp.normalized * moveSpeed * Time.deltaTime;
+            //rigidbody.MovePosition(transform.position + temp);
+        }
+        else
+        {
+            if (speed > decelartion * Time.deltaTime)
+                speed = speed - decelartion * Time.deltaTime;
+            else if (speed < -decelartion * Time.deltaTime)
+                speed = speed + decelartion * Time.deltaTime;
+            else
+                speed = 0;
+        }
+
+        Vector3 temp = new Vector3(-1, 0, 0);
+        temp = temp.normalized * speed * Time.deltaTime;
+        rigidbody.MovePosition(transform.position + temp);
+        //transform.position.x = transform.position.x + speed * Time.deltaTime;
+    }
+
+    private void CheckJump()
+    {
+        if (isGrounded() && Input.GetButtonDown("Jump"))
+        {
+            moveDirection.y = jumpForce;
+            rigidbody.velocity = moveDirection;
+        }
+    }
+
+    private bool isGrounded()
+    {
+        return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f, Ground.value);
+    }
+}

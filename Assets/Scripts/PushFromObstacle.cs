f25a698a220d73d6167abb13eba70d68e9d7cae5
Carlos Andrés Chiale <carloncho@thechfactory.com>
2020-12-10 16:39:00 -0300

    Added more obstacles

---
+++ Assets/Scripts/PushFromObstacle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushFromObstacle : MonoBehaviour
+{
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Vector3 direction = transform.position - player.transform.position;
+        direction.y = 0;
+        direction.z = 0;
+        player.GetComponent<Rigidbody>().AddForce(direction.normalized * 1000, ForceMode.Impulse);
+    }
+}
